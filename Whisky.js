259f4dbd20dccae64766b412f275f1f2a7388cf9
Tim <timjquigg@gmail.com>
2020-12-25 14:25:27 -0700

    Added initial functionality

---
+++ Whisky.js
@@ -0,0 +1,50 @@
+/*  The following object contains the 6 Scotch Whisky Regions along with a count of how many distilleries are in each region.
+    This data was taken from https://en.wikipedia.org/wiki/List_of_whisky_distilleries_in_Scotland on 12/25/2020. */
+let whisky_regions = {
+    'Campbeltown' : 3,
+    'Highland': 36,
+    'Island' : 12,
+    'Islay' : 9,
+    'Lowland' : 16,
+    'Speyside' : 51
+};
+
+// This list contains all of the guests at the whisky night
+let guests = [
+    'Tim',
+    'Geoff',
+    'Caleb',
+    'Elson',
+    'Nathan',
+    'Michael',
+    'Jordan',
+    'David',
+    'John'
+];
+
+/*  This function creates a weighted list of whisky regions. This will be used later so that when regions are randomly
+    assigned, any extra assignments are done weighted towards the regions with more distilleries. */
+let create_weighted_list = regions => {
+    let new_list = [];
+    for (region in regions) {
+        for (let i = 0; i < regions[region]; i++) {
+            new_list.push(region);
+        }
+    }
+    return new_list;
+}
+
+let weighted_list = create_weighted_list(whisky_regions);
+
+// This function will assign a whisky region to each guest.
+let create_assignments = (names_list, regions_list) => {
+    let new_obj = {}
+    names_list.forEach(guest => {
+        let check = Math.floor(Math.random() * regions_list.length + 1);
+        new_obj[guest] = regions_list[check];
+    });
+    return new_obj;
+};
+
+let assignments = create_assignments(guests, weighted_list);
+console.log(assignments);
