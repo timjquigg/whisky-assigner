e274e638845fae603ec95de6151f192e6eb56f4a
Tim <timjquigg@gmail.com>
2020-12-25 20:50:43 -0700

    added check for duplicates

--- Whisky.js
+++ Whisky.js
@@ -40,8 +40,29 @@ let weighted_list = create_weighted_list(whisky_regions);
 let create_assignments = (names_list, regions_list) => {
     let new_obj = {}
     names_list.forEach(guest => {
-        let check = Math.floor(Math.random() * regions_list.length + 1);
-        new_obj[guest] = regions_list[check];
+        // This number will be used to pick a random region out of the weighted list of whisky regions
+        let check = Math.floor(Math.random() * (regions_list.length-1));
+        /*  Since it is desired to have a representative whisky from each whisky region before doubling up, this
+            boolean variable will be used in the check to see if a region is a duplicate in the assignments list. */
+        let unique = false;
+        while (unique === false) {
+            // If this is the first entry in the new_obj assignments, or if all of the regions are already accounted for.
+            if (Object.keys(new_obj).length === 0 || Object.keys(new_obj).length >= Object.keys(whisky_regions).length){
+                new_obj[guest] = regions_list[check];
+                regions_list.splice(check,1); // remove region from weighted list to update weighting.
+                unique = true; // move on to next guest.
+            } else {
+                // If the region is a duplicate in the new_obj and not all regions are accounted for, find a new random region. 
+                if (Object.values(new_obj).indexOf(regions_list[check]) !== -1){
+                    check = Math.floor(Math.random() * (regions_list.length - 1));
+                    regions_list.splice(check,1); // remove region from weighted list to update weigthing.
+                } else {
+                    new_obj[guest] = regions_list[check];
+                    regions_list.splice(check,1);
+                    unique = true; // move on to next guest.
+                }
+            }
+        }
     });
     return new_obj;
 };
