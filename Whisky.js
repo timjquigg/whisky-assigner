76f86c3ec76623650178caf9465a98c1995ff8f2
Tim <timjquigg@gmail.com>
2020-12-27 12:57:05 -0700

    rewrote the program to be easier to follow and edit.

--- Whisky.js
+++ Whisky.js
@@ -47,59 +47,88 @@ const create_weighted_list = regions => {
     }
     return new_list;
 }
-
-const weighted_list = create_weighted_list(whisky_regions);
-
-// This function will assign a whisky region to each guest.
-const create_whisky_assignments = (names_list, regions_list) => {
-    const new_obj = {}
-    names_list.forEach(guest => {
+// This function creates a whisky list that will later be incorporated into the assignments Object.
+const create_whisky_list = (guest_list, regions_list, weighted_list_func) => {
+    let new_list = [];
+    weighted_list = weighted_list_func(regions_list);
+    for (let i = 0; i < guest_list.length; i++) {
         // This number will be used to pick a random region out of the weighted list of whisky regions
-        let check = Math.floor(Math.random() * (regions_list.length-1));
+        let check = Math.floor(Math.random() * (weighted_list.length - 1));
         /*  Since it is desired to have a representative whisky from each whisky region before doubling up, this
             boolean variable will be used in the check to see if a region is a duplicate in the assignments list. */
-        let unique = false;
-        while (unique === false) {
-            // If this is the first entry in the new_obj assignments, or if all of the regions are already accounted for.
-            if (Object.keys(new_obj).length === 0 || Object.keys(new_obj).length >= Object.keys(whisky_regions).length){
-                new_obj[guest] = regions_list[check];
-                regions_list.splice(check,1); // remove region from weighted list to update weighting.
-                unique = true; // move on to next guest.
+        let unique = true;
+        while (unique === true) {
+            // If this is the first entry in the new_list, or if all of the regions are already accounted for.
+            if (new_list.length === 0 || new_list.length >= Object.keys(regions_list).length) {
+                new_list.push(weighted_list[check]);
+                weighted_list.splice(check,1); // remove region entry from weighted list to update weighting.
+                unique = false; // move on to index.
             } else {
-                // If the region is a duplicate in the new_obj and not all regions are accounted for, find a new random region. 
-                if (Object.values(new_obj).indexOf(regions_list[check]) !== -1){
-                    regions_list.splice(check,1); // remove region from weighted list to update weigthing.
-                    check = Math.floor(Math.random() * (regions_list.length - 1));
+                // If the region is a duplicate in the new_list and not all regions are accounted for, find a new random region.
+                if (new_list.indexOf(weighted_list[check]) !== -1) {
+                    weighted_list.splice(check,1);
+                    check = Math.floor(Math.random() * (weighted_list.length -1));
                 } else {
-                    new_obj[guest] = regions_list[check];
-                    regions_list.splice(check,1);
-                    unique = true; // move on to next guest.
+                    new_list.push(weighted_list[check]);
+                    weighted_list.splice(check,1);
+                    unique = false;
                 }
             }
         }
-    });
-    return new_obj;
+    }
+    return new_list;
 };
 
-const create_snack_assignments = (obj, snacks_list) => {
+// This function creates a snack list that will later be incorporated into the assignments Object.
+const create_snack_list = (guest_list, snacks_list) => {
     const new_list = [];
-    for (key in obj) {
+    for (let i = 0; i < guest_list.length; i++) {
+        // This will pick a random snack from the snacks list.
         let check = Math.floor(Math.random() * snacks_list.length);
-        let unique = false;
-        while (unique === false) {
+        let unique = true;
+        while (unique === true) {
+            // If this is the first entry into the new_list or if all snacks are already accounted for.
             if (new_list.indexOf(snacks_list[check]) === -1 || new_list.length >= snacks_list.length){
                 new_list.push(snacks_list[check]);
-                unique = true;
+                unique = false;
             } else {
-                check = Math.floor(Math.random() * snacks_list.length);
+                check = Math.floor(Math.random() * snacks_list.length); // Create a new random snack to check.
             }
         }
-        obj[key] += ',' + new_list[Object.keys(obj).indexOf(key)];
-        obj[key] = obj[key].split(',');
     }
-
+    return new_list;
 }
 
-const assignments = create_whisky_assignments(guests, weighted_list);
-create_snack_assignments(assignments, snacks);
-console.log(assignments);
+// This function creates a draw list that will later be incorporated into the assignments Object.
+const create_draw_list = guest_list => {
+    const draw_numbers = []; // Numbers that will be drawn from
+    const new_list = [];
+    // This will generate the draw numbers from 1 to n where n is the number of guests.
+    for (let i = 1; i <= guest_list.length; i++) {
+      draw_numbers.push(i);  
+    }
+    // Draws a random number from the draw list and assigns it to the new list and then removes from the draw list.
+    let check = Math.floor(Math.random() * draw_numbers.length);
+    while (draw_numbers.length > 0) {
+        new_list.push(draw_numbers[check]);
+        draw_numbers.splice(check,1);
+        check = Math.floor(Math.random() * draw_numbers.length);
+        }
+    return new_list;
+}
+
+/*  This function takes all of the previously generated lists and creates a new Object where the keys are the guest names
+    and the values are from the whisky, snack, and draw lists. */
+const create_assignments = (guest_lst, whisky_lst, snack_lst, draw_lst) => {
+    const new_obj = {};
+    for (let i = 0; i < guest_lst.length; i++) {
+        new_obj[guest_lst[i]] = [whisky_lst[i], snack_lst[i], draw_lst[i]];
+    };
+    return new_obj;
+}
+
+const whisky_list = create_whisky_list(guests, whisky_regions, create_weighted_list);
+const snack_list = create_snack_list(guests, snacks);
+const draw_list = create_draw_list(guests);
+
+console.log(create_assignments(guests, whisky_list, snack_list, draw_list));
